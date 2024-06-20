d5516674bc4760d83b5f2df0458aaa830d10d02a
akshitha-108 <akshithajnv@gmail.com>
2024-06-20 18:58:25 +0530

    adding frontend

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;

3f7da80fef26f3152ea0d4c7685b039f82198019
akshitha-108 <akshithajnv@gmail.com>
2024-06-20 19:29:39 +0530

    modified about

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// this is about.js 
+//and iam testing the conflicts
+//iam editing to check in vscode to do the command stash
+
 const About = () => {
   return (
     <div>
